b80dab2f1519ab92f14bfacd8d39e8a6d4390dad
DinhHieu2004 <22130082@st.hcmuaf.edu.vn>
2026-01-09 16:34:42 +0700

    add testSumWithVeryBig function

--- src/test/java/add2Num/MyBigNumberTest.java
+++ src/test/java/add2Num/MyBigNumberTest.java
@@ -1,5 +1,6 @@
 package add2Num;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
@@ -17,23 +18,30 @@ class MyBigNumberTest {
     }
 
     @Test
-    // Cọng 2 số có cùng độ dài không nhớ
+    @DisplayName(" // Cọng 2 số có cùng độ dài không nhớ")
     void testSumNormal() {
         assertEquals("227", myBigNumber.sum("113", "114"));
     }
 
     @Test
-    // Cọng 2 số khác độ dài
+    @DisplayName(" // Cọng 2 số khác độ dài")
+
     void testSumDifferentLength() {
         assertEquals("2131", myBigNumber.sum("1234", "897"));
     }
 
     @Test
-    // Cọng 2 số có nhớ
+    @DisplayName(" // Cọng 2 số có nhớ")
     void testSumWithCarry() {
         assertEquals("1356", myBigNumber.sum("789", "567"));
     }
 
-
+    @Test
+    @DisplayName("// Cong 1 số rất lớn cho 1 số rất nhỏ")
+    void testSumWithVeryBig() {
+        String stn1 = "999999999999";
+        String stn2 = "1";
+        assertEquals("1000000000000", myBigNumber.sum(stn1, stn2));
+    }
 
 }
